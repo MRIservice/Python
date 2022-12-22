cccfc58109cd7ef5398745f9d7e9d592d7a969d2
davidh <mriservice@gmail.com>
2022-12-22 11:28:01 -0500

    Added MailMerge

---
+++ MailMerge/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# This is a simple Python MailMerge program.
+
+STRING = "[name]"
+
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter.replace(STRING, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", "w") as completed_letter:
+            completed_letter.write(new_letter)
